c83860726e89ad498e3b3122ad84b385c55c5f38
qfisch <quentin.fisch@sensirion.com>
2026-01-16 10:46:06 +0100

    Added example for DeviceInfo service

---
+++ examples/BleGadgetWithDeviceInfo/BleGadgetWithDeviceInfo.ino
@@ -0,0 +1,59 @@
+#include "SensirionUptBleServer.h"
+#include "DeviceInformationBleService.h"
+
+using namespace sensirion::upt;
+
+ble_server::NimBLELibraryWrapper lib;
+ble_server::DeviceInformationBleService deviceInfoBleService(lib);
+ble_server::UptBleServer uptBleServer(lib, core::T_RH_CO2_ALT);
+
+uint16_t t = 0;
+uint16_t rh = 0;
+uint16_t co2 = 0;
+
+static int64_t lastMeasurementTimeMs = 0;
+static int measurementIntervalMs = 1000;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000); // Wait for Serial monitor to start
+
+  // setup device information ble services
+  deviceInfoBleService.setFirmwareRevision("1.2.3");
+  deviceInfoBleService.setManufacturerName("DiY Corp");
+  deviceInfoBleService.setModelNumber("MyGadget rev1");
+  uptBleServer.registerBleServiceProvider(deviceInfoBleService);
+
+  // Initialize the GadgetBle Library
+  uptBleServer.begin();
+
+  Serial.print("Sensirion GadgetBle Lib initialized with deviceId = ");
+  Serial.println(uptBleServer.getDeviceIdString());
+}
+
+void loop() {
+  if (millis() - lastMeasurementTimeMs >= measurementIntervalMs) {
+    uptBleServer.writeValueToCurrentSample(
+        ++t % 50, core::SignalType::TEMPERATURE_DEGREES_CELSIUS);
+    uptBleServer.writeValueToCurrentSample(
+        ++rh % 100, core::SignalType::RELATIVE_HUMIDITY_PERCENTAGE);
+    uptBleServer.writeValueToCurrentSample(
+        ++co2 % 1000, core::SignalType::CO2_PARTS_PER_MILLION);
+    uptBleServer.commitSample();
+    lastMeasurementTimeMs = millis();
+    // Provide the sensor values for Tools -> Serial Monitor or Serial Plotter
+    Serial.print("mockCO2[ppm]:");
+    Serial.print(co2);
+    Serial.print("\t");
+    Serial.print("mockTemperature[℃]:");
+    Serial.print(t);
+    Serial.print("\t");
+    Serial.print("mockHumidity[%]:");
+    Serial.println(rh);
+  }
+
+  // handle download requests
+  uptBleServer.handleDownload();
+
+  delay(20);
+}
